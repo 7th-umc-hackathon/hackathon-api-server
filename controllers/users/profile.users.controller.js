d02e978945fa96a28c9f5260130f28424c792d01
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-01-12 03:55:20 +0900

    Feat: 개인, 국가 랭킹 반환

--- controllers/users/profile.users.controller.js
+++ controllers/users/profile.users.controller.js
@@ -1,1 +1,37 @@
+import * as userService from "../../services/users/users.service.js";
+import { logError } from "../../utils/handlers/error.logger.js";
 
+export const userProfile = async (req, res, next) => {
+  try {
+    const user = await userService.userProfile(req.user.user_id);
+
+    return res.status(200).success({ user });
+  } catch (err) {
+    logError(err);
+    next(err);
+  }
+};
+
+export const userRank = async (req, res, next) => {
+  try {
+    const rank = await userService.userRanking(req.user.user_id);
+
+    return res.status(200).success({ rank });
+  } catch (err) {
+    logError(err);
+    next(err);
+  }
+};
+
+export const countryRank = async (req, res, next) => {
+  try {
+    const rank = await userService.countryRanking(req.user.country_id);
+
+    return res.status(200).success({ rank });
+  } catch (err) {
+    logError(err);
+    next(err);
+  }
+};
+
+export const claimRewared = async (req, res, next) => {};
--- routers/users.router.js
+++ routers/users.router.js
@@ -1,7 +1,31 @@
 import express from "express";
+import * as authMiddleware from "../middleware/authenticate.jwt.js";
+import * as profileController from "../controllers/users/profile.users.controller.js";
 
 const router = express.Router();
 
 router.get("/", (req, res) => res.success({ message: "Good To Go" }));
+
+// 사용자 프로필 조회
+router.get(
+  "/profile",
+  authMiddleware.authenticateAccessToken,
+  profileController.userProfile
+);
+
+router.get(
+  "/rank/user",
+  authMiddleware.authenticateAccessToken,
+  profileController.userRank
+);
+
+router.get(
+  "/rank/country",
+  authMiddleware.authenticateAccessToken,
+  profileController.countryRank
+);
+
+// 리워드 수령
+router.patch("/:relay_id/claim-reward", authMiddleware.authenticateAccessToken);
 
 export default router;

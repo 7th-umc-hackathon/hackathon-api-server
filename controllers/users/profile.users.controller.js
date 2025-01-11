4adef036e4d6a2dbaa7f8e31d73aa09cba01482e
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-01-12 04:33:56 +0900

    Feat: 랭킹류 개선

--- controllers/users/profile.users.controller.js
+++ controllers/users/profile.users.controller.js
@@ -23,9 +23,31 @@ export const userRank = async (req, res, next) => {
   }
 };
 
-export const countryRank = async (req, res, next) => {
+export const countryRankingList = async (req, res, next) => {
   try {
-    const rank = await userService.countryRanking(req.user.country_id);
+    const rank = await userService.countryRanking();
+
+    return res.status(200).success({ rank });
+  } catch (err) {
+    logError(err);
+    next(err);
+  }
+};
+
+export const myCountryRanking = async (req, res, next) => {
+  try {
+    const rank = await userService.myCountryRanking(req.user.user_id);
+
+    return res.status(200).success({ rank });
+  } catch (err) {
+    logError(err);
+    next(err);
+  }
+};
+
+export const userRankList = async (req, res, next) => {
+  try {
+    const rank = await userService.userRankList();
 
     return res.status(200).success({ rank });
   } catch (err) {
